8fa05bd6f4bcf2f5dca2a830ea4bc6b81ed3aa3c
Adilson <adilson.kedeca@gmail.com>
2022-05-02 13:08:37 -0300

    Mensagens Personalizadas

---
+++ src/main/java/com/example/demo/resources/exceptions/ValidationError.java
@@ -0,0 +1,27 @@
+package com.example.demo.resources.exceptions;
+
+import java.util.List;
+
+public class ValidationError extends  StandardError{
+
+    private List<FieldMessage> errors;
+
+
+    public ValidationError(int status, String msg, Long timeStamp) {
+        super(status, msg, timeStamp);
+        //TODO Auto-generated constructor stub
+    }
+
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+
+
+    
+}
